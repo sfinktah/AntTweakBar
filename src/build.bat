8c13f77487e8ec975dc7863962ca126a730f6141
Sfinktah Bungholio <sfinktah@github.spamtrak.org>
2016-08-25 23:10:49 +1000

    added demo UI from socialclub

--- src/build.bat
+++ src/build.bat
@@ -1,2 +1,2 @@
 call "%VS140COMNTOOLS%\..\..\VC\vcvarsall.bat" x64
-msbuild /p:Configuration=Release /clp:Summary /nologo AntTweakBar_VS2015.sln
+msbuild /p:Configuration=Release /m /clp:Summary /nologo AntTweakBar_VS2015.sln
